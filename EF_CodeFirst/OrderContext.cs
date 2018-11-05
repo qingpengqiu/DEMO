73232ed85bbbe23fb81fa58631102d1baf14c6bf
qingpengqiu <qingpengqiu@163.com>
2018-11-06 00:15:19 +0800

    181105

---
+++ EF_CodeFirst/OrderContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using EF_CodeFirst.Model;
+
+namespace EF_CodeFirst
+{
+    class OrderContext : DbContext
+    {
+        public OrderContext(string connectionstring) : base(connectionstring)
+        { }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+    }
+}
--- EF_CodeFirst/Program.cs
+++ EF_CodeFirst/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EF_CodeFirst.Model;
 
 namespace EF_CodeFirst
 {
@@ -10,6 +11,24 @@ namespace EF_CodeFirst
     {
         static void Main(string[] args)
         {
+            using (var orderDBContext = new OrderContext("CodeFirstDb"))
+            {
+                var order = new Order();
+                order.OrderDate = DateTime.Now;
+                order.Customer = "qiuqp";
+                orderDBContext.Orders.Add(order);
+                orderDBContext.SaveChanges();
+
+
+                var Orders = from theorder in orderDBContext.Orders
+                             select theorder;
+                foreach (var item in Orders)
+                {
+                    Console.WriteLine("OrderId:{0};OrderDate:{1}", item.Id, item.OrderDate);
+                }
+                Console.ReadLine();
+              
+            }
         }
     }
 }
