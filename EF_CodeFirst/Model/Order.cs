9d8d3b1bc21b5d5fd2f7c2239a4f2aca0f318046
unknown <qiuqp@digitalchina.com>
2018-11-05 18:01:36 +0800

    2018-11-05

---
+++ EF_CodeFirst/Model/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_CodeFirst.Model
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public string Customer { get; set; }
+
+        public DateTime OrderDate { get; set; }
+
+        public virtual List<OrderDetail> OrderDetails { get; set; }
+    }
+}
